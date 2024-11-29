21bd587df4f9171082f0ed8da269bde9aaa8f99d
GLOIRE5 <g.gwiza@alustudent.com>
2024-11-29 14:38:57 -0800

    changes

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ fetch 'https://alu-intranet.hbtn.io/status'"""
 import urllib.request
+
+
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
