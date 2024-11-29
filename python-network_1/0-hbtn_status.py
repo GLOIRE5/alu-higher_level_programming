ba97be84dba6d910c962b41b51e0f4af8615c2f2
GLOIRE5 <g.gwiza@alustudent.com>
2024-11-29 19:46:16 +0100

    fixed issue

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-""" fetch 'https://alu-intranet.hbtn.io/status'"""
+""" fetch 'http://0.0.0.0:5050/status' """
 import urllib.request
 
-
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    # Corrected URL with 'status' instead of 'statu'
+    url = "http://0.0.0.0:5050/status"
+    
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    except Exception as e:
+        print(f"Failed to fetch the URL: {e}")
+
