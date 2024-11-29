8b8843914392c506ec2a9b5c54314c907baab249
GLOIRE5 <g.gwiza@alustudent.com>
2024-11-29 14:46:38 -0800

    fixed

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,9 +7,6 @@ import requests
 if __name__ == '__main__':
 
     res = requests.get("https://intranet.hbtn.io/status")
-    
-    # Print the response details
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))  # Type of the response
-    print("\t- content: {}".format(res.text))    # Content of the response
-    print("\t- utf8 content: {}".format(res.text))  # UTF-8 content (same as text)
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
