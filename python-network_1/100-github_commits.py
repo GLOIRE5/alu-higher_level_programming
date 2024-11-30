f6a2fa9dcce83e471f182a02e771a8883af00bc4
GLOIRE5 <g.gwiza@alustudent.com>
2024-11-30 06:59:21 -0800

    fixed

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-import requests
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
 import sys
+import requests
 
-def get_commits(repo_name, owner_name):
-  """Fetches and prints 10 commits from a GitHub repository.
-
-  Args:
-    repo_name: The name of the repository.
-    owner_name: The owner of the repository.
-  """
-  url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-  headers = {'Accept': 'application/json'}
-  params = {'per_page': 10}
-
-  response = requests.get(url, headers=headers, params=params)
-  response.raise_for_status()
-
-  for commit in response.json():
-    sha = commit['sha']
-    author_name = commit['commit']['author']['name']
-    print(f"{sha}: {author_name}")
 
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print("Usage: python 100-github_commits.py <repo_name> <owner_name>")
-    sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-  repo_name = sys.argv[1]
-  owner_name = sys.argv[2]
-
-  get_commits(repo_name, owner_name)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
