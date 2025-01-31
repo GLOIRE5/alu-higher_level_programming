8e9b3c386cb402bc5dbc74d51cd0bb0ff1b264ad
GLOIRE5 <g.gwiza@alustudent.com>
2025-01-31 15:37:26 +0200

    made same changes

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
+let count = 0;
+
 exports.logMe = function (item) {
-  if (!this.count) {
-    this.count = 0;
-  }
-  console.log(`${this.count++}: ${item}`);
+  console.log(`${count++}: ${item}`);
 };
 
