b647994bfbb895cb8ec3856b136cd8ccea953f61
GLOIRE5 <g.gwiza@alustudent.com>
2025-02-07 12:59:19 +0200

    changes

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,9 @@
-#!/usr/bin/node
-
-const request = require('request');
-
-const id = process.argv[2];
-
 request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, function (err, res) {
   if (err) {
     console.log(err);
   } else {
-    console.log(JSON.parse(res.body).title);
+    console.log(res.body); 
+    console.log(JSON.parse(res.body).title);  
   }
 });
+
